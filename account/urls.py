0e3a8c96799f73641e7aa3d987500058b683d89f
Kalys <jsvsjsvshsksb@gmail.com>
2022-12-22 20:33:51 +0600

    Movies

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import RegisterUserView, activate_view
+from .views import RegisterUserView, activate_view, LoginView, LogoutView
 
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view()),
-    path('activate/<str:activation_code>/', activate_view)
+    path('activate/<str:activation_code>/', activate_view),
+    path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    
 ]
